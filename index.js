8866641b3d4d42a096ae44e8a71e1f9c4c47c4d3
ConradGuillaume <conrad.guillaume0501@gmail.com>
2022-06-26 19:28:33 +0200

    go

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const bigOne = document.querySelector(".select_1");
+const bigTwo = document.getElementById("mod");
+const bigThree = document.getElementById("maq");
 
 bigOne.addEventListener("click", () => {
   bigOne.classList.toggle("get-big");
+});
+
+bigTwo.addEventListener("click", () => {
+  bigTwo.classList.toggle("get-big");
+});
+bigThree.addEventListener("click", () => {
+  bigThree.classList.toggle("get-big");
 });
